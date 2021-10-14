f75c631078d3dd6b144aab91296f69772a0ef53d
KristenSantos <Kristensxntos@gmail.com>
2021-10-13 18:49:47 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+$('.navItem').css("color", "white");
+
+$('#nav').css("background-color", "black");
+
+$('.even').css("background-color", "#ccc");
+
+$('.odd').css("background-color", "#aaaaaa")
+
+$('#myInput').val("Kristen");
+
+
